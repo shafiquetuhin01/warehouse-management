7b7a5354a8290c35d7315f9cbe52c49a5b6d3bb3
Shafique <sfqtuhin04@gmail.com>
2022-05-07 10:08:40 +0600

    updating my assignment 11

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import "./App.css";
-import app from "./firebase.init";
-import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
-const auth = getAuth(app);
 
 function App() {
-  const provider = new GoogleAuthProvider();
-  const fbProvider = new FacebookAuthProvider();
-  const handleSignIn = () => {
-    signInWithPopup(auth, provider)
-    .then(result=>{
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error=>{
-      console.error('tumi error khaiso',error);
-    })
-  };
-  const handleSignInFb = () => {
-    signInWithPopup(auth, fbProvider)
-    .then(result=>{
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error=>{
-      console.error('tumi error khaiso fb te',error);
-    })
-  };
+  
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center" }}>I Love my family</h1>
-      <br />
-      <div style={{ textAlign: "center",padding:'10px ' }}>
-        <button onClick={handleSignIn}>Google Sign In</button>
-        <button onClick={handleSignInFb}>Facebook Sign In</button>
-      </div>
+      
     </div>
   );
 }
---
+++ src/Shared/Header/Login/Login.js
@@ -0,0 +1,49 @@
+import React from "react";
+import app from "./firebase.init";
+import {
+  FacebookAuthProvider,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const auth = getAuth(app);
+
+
+
+const Login = () => {
+  const provider = new GoogleAuthProvider();
+  const fbProvider = new FacebookAuthProvider();
+  const handleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error("tumi error khaiso", error);
+      });
+  };
+  const handleSignInFb = () => {
+    signInWithPopup(auth, fbProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error("tumi error khaiso fb te", error);
+      });
+  };
+  return (
+    <div>
+      <h2>This is login page</h2>
+      <div style={{ textAlign: "center", padding: "10px " }}>
+        <button onClick={handleSignIn}>Google Sign In</button>
+        <button onClick={handleSignInFb}>Facebook Sign In</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
